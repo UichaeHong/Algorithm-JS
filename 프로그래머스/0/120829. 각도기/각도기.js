88ecfda2a45bb7c8a9a70c36b95106717af2bb5b
hong_hong <127120819+UichaeHong@users.noreply.github.com>
2024-05-13 14:42:07 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    if(angle>= 180 || angle >= 0) {
+        if(angle === 90) {
+            return 2
+        } else if(angle === 180) {
+          return 4  
+        } else if(angle > 90) {
+            return 3
+        } else {
+            return 1
+        }
+    }
+}
